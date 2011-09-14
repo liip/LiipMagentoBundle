1bf9eff2a7e5d6f8e563ad8d3fd5619db13ec42b
Lukas Kahwe Smith <smith@pooteeweet.org>
2011-09-14 14:11:27 +0200

    fixed doc comment

--- StoreResolver/StoreResolverInterface.php
+++ StoreResolver/StoreResolverInterface.php
@@ -9,7 +9,8 @@ interface StoreResolverInterface
     /**
      * Resolves the Magento store code to be initiated
      *
-     * @return  \Symfony\Component\HttpFoundation\Request $request
+     * @param   Request $request
+     * @return  string|false    The resolved store code or false if no mapping found
      */
     function resolve(Request $request);
 }
