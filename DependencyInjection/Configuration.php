5581987983dc44a28f2f1660ffe61f4d01bf0085
Fabian Vogler <fabian@equivalence.ch>
2011-09-14 10:55:32 +0200

    Adding DependencyInjection Extension

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Liip\MagentoBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder,
+    Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * Generates the configuration tree.
+     *
+     * @return TreeBuilder
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('liip_magento');
+
+        $rootNode
+            ->children()
+                ->scalarNode('file')->isRequired()->cannotBeEmpty()->end()
+            ->end();
+
+        return $treeBuilder;
+    }
+}
---
+++ DependencyInjection/LiipMagentoExtension.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Liip\MagentoBundle\DependencyInjection;
+
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Config\Definition\Processor;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class LiipMagentoExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $processor = new Processor();
+        $configuration = new Configuration();
+        $config = $processor->processConfiguration($configuration, $configs);
+
+        $this->loadDefaults($container);
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getXsdValidationBasePath()
+    {
+        return __DIR__ . '/../Resources/config/schema';
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getNamespace()
+    {
+        return 'http://symfony.com/schema/dic/liip_magento';
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    protected function loadDefaults($container)
+    {
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+
+        foreach ($this->resources as $resource) {
+            $loader->load($resource);
+        }
+    }
+}
