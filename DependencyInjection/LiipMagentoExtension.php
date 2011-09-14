69c88e1d8027f6173b3309b714e0b3435ef77ae2
Lukas Kahwe Smith <smith@pooteeweet.org>
2011-09-14 11:00:50 +0200

    fixed up the Extension

--- DependencyInjection/LiipMagentoExtension.php
+++ DependencyInjection/LiipMagentoExtension.php
@@ -5,35 +5,24 @@ namespace Liip\MagentoBundle\DependencyInjection;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\Config\Definition\Processor;
-use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class LiipMagentoExtension extends Extension
 {
+    private $resources = array(
+        'services.xml'
+    );
+
     public function load(array $configs, ContainerBuilder $container)
     {
         $processor = new Processor();
         $configuration = new Configuration();
         $config = $processor->processConfiguration($configuration, $configs);
 
+        $container->setParameter('liip_magento.mage_file', $config['file']);
+
         $this->loadDefaults($container);
-    }
-
-    /**
-     * @codeCoverageIgnore
-     */
-    public function getXsdValidationBasePath()
-    {
-        return __DIR__ . '/../Resources/config/schema';
-    }
-
-    /**
-     * @codeCoverageIgnore
-     */
-    public function getNamespace()
-    {
-        return 'http://symfony.com/schema/dic/liip_magento';
     }
 
     /**
